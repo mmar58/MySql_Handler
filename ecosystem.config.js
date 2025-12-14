fda87ae8c347a837f33e435e7a142ad5605ae503
mmar58 <rahmanapu118@gmail.com>
2025-12-14 14:48:13 +0600

    Created ecosystem config js for pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    apps: [{
+        name: "mysql_handler",
+        script: "./src/server.js",
+        watch: false,
+        env: {
+            PORT: 3333,
+            CORS_ORIGIN: "*",
+            DB_CONNECTION_TIMEOUT: 60000,
+            DB_ACQUIRE_TIMEOUT: 60000,
+            MAX_ROWS_PER_PAGE: 1000,
+            DEFAULT_PAGE_SIZE: 100
+        }
+    }]
+};
